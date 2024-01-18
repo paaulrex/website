751ac083c5ed3b7fceb5923526ef87853c550453
Paul De Silos <pauldesilos@gmail.com>
2024-01-17 23:47:35 -0600

    overhaul change of webpage

---
+++ script.js
@@ -0,0 +1,50 @@
+const showAboutMe = document.querySelector('#p1')
+const showGithub = document.querySelector('#p2')
+const showLinkedIn = document.querySelector('#p3')
+const showFootnote = document.querySelector('#p4')
+const mainShow = document.querySelector('.main-section')
+const aboutMeSection = document.querySelector('.about-me')
+
+showAboutMe.onmouseover = function() {
+    showAboutMe.innerHTML = "About Me"
+}
+
+showAboutMe.onmouseout = function () {
+    showAboutMe.innerHTML = "Hi"
+}
+
+showGithub.onmouseover = function() {
+    showGithub.innerHTML = "GitHub"
+}
+
+showGithub.onmouseout = function() {
+    showGithub.innerHTML = "I'm"
+}
+
+showLinkedIn.onmouseover = function() {
+    showLinkedIn.innerHTML = "LinkedIn"
+}
+
+showLinkedIn.onmouseout = function() {
+    showLinkedIn.innerHTML = "Paul"
+}
+
+showFootnote.onmouseover = function() {
+    showFootnote.innerHTML = "If you're a millenial, you probably said it like the monkey in Jimmy Neutron credits. 👀"
+    showFootnote.style.color = "whitesmoke"
+}
+
+showFootnote.onmouseout = function() {
+    showFootnote.innerHTML = "-------------------------------------"
+    showFootnote.style.color = "transparent"
+}
+
+function showMore() {
+    mainShow.style.display = "none"
+    aboutMeSection.style.display = "block"
+}
+
+function backToMain() {
+    mainShow.style.display = "flex"
+    aboutMeSection.style.display = "none"
+}
