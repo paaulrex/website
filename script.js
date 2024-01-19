248d50ce8c5d66cf1005c55bb54ea82d57061714
Paul De Silos <pauldesilos@gmail.com>
2024-01-18 20:59:35 -0600

    disabled js script for dev width < 800

--- script.js
+++ script.js
@@ -5,43 +5,15 @@ const showFootnote = document.querySelector('#p4')
 const mainShow = document.querySelector('.main-section')
 const aboutMeSection = document.querySelector('.about-me')
 
-showAboutMe.onmouseover = function() {
-    showAboutMe.innerHTML = "About Me"
-}
-
-showAboutMe.onmouseout = function () {
-    showAboutMe.innerHTML = `Hi <span class="mobile-view-text">// About Me</span>`
-}
-
-showGithub.onmouseover = function() {
-    showGithub.innerHTML = "GitHub"
-}
-
-showGithub.onmouseout = function() {
-    showGithub.innerHTML = `I'm <span class="mobile-view-text">// GitHub</span>`
-}
-
-showLinkedIn.onmouseover = function() {
-    showLinkedIn.innerHTML = "LinkedIn"
-}
-
-showLinkedIn.onmouseout = function() {
-    showLinkedIn.innerHTML = `Paul <span class="mobile-view-text">// LinkedIn</span>`
-}
-
-showFootnote.onmouseover = function() {
-    showFootnote.innerHTML = "If you're a millenial, you probably said it like the monkey in Jimmy Neutron credits. 👀"
-    showFootnote.style.color = "whitesmoke"
-}
-
-showFootnote.onmouseout = function() {
-    showFootnote.innerHTML = "---------------------------------"
-    showFootnote.style.color = "transparent"
-}
-
 function showMore() {
     mainShow.style.display = "none"
     aboutMeSection.style.display = "block"
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) && window.innerwidth < 800 ) {
+        const footer = document.querySelector('footer')
+        footer.style.position = "relative"
+    } else {
+
+    }
 }
 
 function backToMain() {
@@ -51,8 +23,40 @@ function backToMain() {
     footer.style.position = "absolute"
 }
 
-showAboutMe.addEventListener("click", function() {
-    const footer = document.querySelector('footer')
-    footer.style.position = "relative"
-})
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    
+} else {
+    showAboutMe.onmouseover = function() {
+        showAboutMe.innerHTML = "About Me"
+    }
 
+    showAboutMe.onmouseout = function () {
+        showAboutMe.innerHTML = `Hi <span class="mobile-view-text">// About Me</span>`
+    }
+
+    showGithub.onmouseover = function() {
+        showGithub.innerHTML = "GitHub"
+    }
+
+    showGithub.onmouseout = function() {
+        showGithub.innerHTML = `I'm <span class="mobile-view-text">// GitHub</span>`
+    }
+
+    showLinkedIn.onmouseover = function() {
+        showLinkedIn.innerHTML = "LinkedIn"
+    }
+
+    showLinkedIn.onmouseout = function() {
+        showLinkedIn.innerHTML = `Paul <span class="mobile-view-text">// LinkedIn</span>`
+    }
+
+    showFootnote.onmouseover = function() {
+        showFootnote.innerHTML = "If you're a millenial, you probably said it like the monkey in Jimmy Neutron credits. 👀"
+        showFootnote.style.color = "whitesmoke"
+    }
+
+    showFootnote.onmouseout = function() {
+        showFootnote.innerHTML = "---------------------------------"
+        showFootnote.style.color = "transparent"
+    }
+}
